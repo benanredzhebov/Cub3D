31a512dd812b64f4b853de645587dd02ed76da27
benanredzhebov <benanredzhebov@gmail.com>
2024-08-19 17:43:59 +0200

    added comment to set_image_function

--- src/image.c
+++ src/image.c
@@ -6,18 +6,22 @@
 /*   By: beredzhe <beredzhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 12:14:38 by beredzhe          #+#    #+#             */
-/*   Updated: 2024/08/19 12:16:10 by beredzhe         ###   ########.fr       */
+/*   Updated: 2024/08/19 17:42:34 by beredzhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/raytracer.h"
 
+/*calculates the position of the pixel within the image's
+linear array(addr) that stores pixel data
+*/
 void	set_image_pixel(t_img *image, int x, int y, int color)
 {
 	int	pixel;
 
 	pixel = y * (image->size_line / 4) + x;
 	image->addr[pixel] = color;
+	printf("size_line = %d\n", image->size_line);
 }
 
 void	init_img(t_data *data, t_img *image, int width, int height)
